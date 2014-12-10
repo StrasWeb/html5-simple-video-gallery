6c81c6e1de6f9aafaf55cad11ac1f1c5ffd11e58
Pierre Rudloff <contact@rudloff.pro>
2014-12-10 14:44:51 +0100

    Initial commit

---
+++ html5-simple-video-gallery.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Use OpenStreetMap in WordPress Events Manager
+ * 
+ * PHP Version 5.4
+ * 
+ * @category Plugin
+ * @package  HTML5SimpleVideoGallery
+ * @author   StrasWeb <contact@strasweb.fr>
+ * @license  GPL http://www.gnu.org/licenses/gpl.html
+ * @link     https://github.com/StrasWeb/html5-simple-video-gallery
+ */
+/*
+Plugin Name: HTML5 Simple Video Gallery
+Plugin URI: https://github.com/StrasWeb/html5-simple-video-gallery
+Description: Shortcodes that help build a simple video gallery for WordPress
+Author: StrasWeb
+Version: 0.1
+Author URI: https://strasweb.fr/
+*/
+
+/**
+ * Display a gallery in a page
+ * 
+ * @param array $atts Shortcode attributes
+ * 
+ * @return void
+ * */
+function HTML5VideoGallery($atts)
+{
+    $posts = get_posts(array('category'=>$atts['cat']));
+    foreach ($posts as $post) {
+        $date = new DateTime($post->post_date);
+        $duration = get_post_meta($post->ID, 'duration', true);
+        echo '<div class="html5_video_gallery_video">
+            <a class="html5_video_gallery_link"
+            href="'.get_permalink($post->ID).'">';
+        echo '<div class="html5_video_gallery_thumb">',
+            get_the_post_thumbnail($post->ID, 'medium'), '</div>';
+        echo '<h4 class="html5_video_gallery_title">', $post->post_title;
+        if (!empty($duration)) {
+            echo ' <small>(',
+                get_post_meta($post->ID, 'duration', true),
+                ')</small>';
+        }
+        echo '</h4>';
+        echo '<div>', $date->format('d/m/Y'), '</div>';
+        echo '</a></div>';
+    }
+}
+
+/**
+ * Display an HTML5 video player
+ * 
+ * @param array $atts Shortcode attributes
+ * 
+ * @return void
+ * */
+function HTML5VideoPost($atts)
+{
+    echo '<video controls src="'.$atts['url'].'"></video>';
+}
+
+add_shortcode(html5_video_gallery, 'HTML5VideoGallery');
+add_shortcode(html5_video_post, 'HTML5VideoPost');
+wp_enqueue_style(
+    'html5-simple-video-gallery',
+    plugins_url('css/main.css', __FILE__)
+);
+?>
---
+++ readme.txt
@@ -0,0 +1,25 @@
+=== Plugin Name ===
+Contributors: Rudloff
+Tags: video, gallery, html5
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl.html
+Requires at least: 4.0
+Tested up to: 4.0
+
+Shortcodes that help build a simple video gallery for WordPress
+
+== Description ==
+
+This plugin adds two shortcodes html5_video_gallery and html5_video_post that help you integrate a simple HTML5 video gallery into your website.
+
+== Installation ==
+1. Upload the `events-manager-osm` directory to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Add the [html5_video_gallery cat="myCategoryNumber"] shortcode to your gallery page
+1. Add the [html5_video_post url="myVideoUrl"] shortcode to your video pages
+
+== Changelog ==
+
+= 0.1 =
+* Initial release
